e1c5e297549a30902b641bf3129d53ffc1d5d6ef
Laura <laurapapa693@gamil.com>
2026-06-01 10:26:46 -0300

    Movimentacao no banco

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models import categoria
 from app.models import produto
 from app.models import usuario
+from app.models import movimentacao
 
-#python -m alembic revision --autogenerate -m "Criar tabelas categorias e produtos"
+#python -m alembic revision --autogenerate -m "Criando tabela movimentacao"
 #python -m alembic upgrade head
