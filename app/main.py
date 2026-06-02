bdbdec6fa0dee0a70dea0d8c05aa83aa235e1f3d
Laura <laurapapa693@gmail.com>
2026-06-02 10:40:47 -0300

    feat: Implement sales and items models with database migrations
    - Created `Venda` and `ItemVenda` models to represent sales and their items.
    - Added relationships between sales and clients, as well as sales and items.
    - Developed HTML templates for client forms, client index, sales receipt, and sales history.
    - Designed the main PDV interface with product listing and shopping cart functionality.
    - Created database migrations for the new sales and clients tables.

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ from app.controllers import admin_controller
 from app.controllers import categoria_controller
 from app.controllers import produto_controller
 from app.controllers import movimentacao_controller
+from app.controllers import pvd_controller
+from app.controllers import cliente_controller
 from app.auth import get_usuario_opcional
 
 app = FastAPI(title="Sistema Estoque")
@@ -24,7 +26,8 @@ app.include_router(admin_controller.router)
 app.include_router(categoria_controller.router)
 app.include_router(produto_controller.router)
 app.include_router(movimentacao_controller.router)
-
+app.include_router(pvd_controller.router)
+app.include_router(cliente_controller.router)
 
 
 
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,8 @@ from app.models import categoria
 from app.models import produto
 from app.models import usuario
 from app.models import movimentacao
+from app.models import cliente
+from app.models import vendas
 
 #python -m alembic revision --autogenerate -m "Criando tabela movimentacao"
 #python -m alembic upgrade head
